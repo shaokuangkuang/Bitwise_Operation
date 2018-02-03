5a0d8944bf3162f2b7970a9443f5caf173d02e6d
Janking <JQPluto@outlook.com>
2018-02-03 18:42:21 +0800

    add

---
+++ 位运算/位运算/位取反.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	unsigned char ch = 15;// 0000 1111
+	unsigned char fch = ~ch;//1111 0000
+
+}
---
+++ 位运算/位运算/位运算.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main1()
+{
+	unsigned char ch = 0;
+	printf("%d\n", sizeof(ch));
+	printf("%p\n", &ch);
+	//可以通过赋值操作二进制
+	system("pause");
+
+}
